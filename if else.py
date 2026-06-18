53bbde0cb05842ae10fc4a3a81027f14f50888de
rizkialfikrisaputra-glitch <rizkialfikrisaputra@gmail.com>
2026-06-18 23:07:05 +0700

    Add if-else statement for user name responses

---
+++ if else.py
@@ -0,0 +1,13 @@
+# if else statement
+nama=input("siapa nama anda?")
+#if kondiisi: aksi
+
+if nama=="Fahzan": print("kamu anime anjay")
+print(f"terimakasih {nama}")
+
+
+if nama=="sasuke":
+  print("anjay cool bet")
+  print("keren bet king")
+  print(f"terimakasih {nama}")
+  
