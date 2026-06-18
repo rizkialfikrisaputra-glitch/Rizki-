6bfd52504c89e3612dd15cf425e7c879fd6181fe
rizkialfikrisaputra-glitch <rizkialfikrisaputra@gmail.com>
2026-06-18 22:51:27 +0700

    Create atribut dalam class.py

---
+++ atribut dalam class.py
@@ -0,0 +1,30 @@
+class AlatMusik:
+    nama1 = "Gitar"
+    asal1 = "Amerika"
+    terbuat1 = "Kayu"
+    cara_dimainkan1 = "Digenjreng atau di petik"
+    jenis_alatmusik1="modern"
+    
+    nama2 = "drum"
+    asal2 = "jawa"
+    terbuat2 = "kayu dan kulit juga plastik"
+    cara_dimainkan2 = "Dipukul"
+    jenis_alatmusik2="modern"
+    
+alat1 = AlatMusik()
+alat2 = AlatMusik()
+
+print("Alat 1:")
+print(alat1.nama1)
+print(alat1.asal1)
+print(alat1.terbuat1)
+print(alat1.cara_dimainkan1)
+print(alat1.jenis_alatmusik1)
+print()
+
+print("Alat 2:")
+print(alat2.nama2)
+print(alat2.asal2)
+print(alat2.terbuat2)
+print(alat2.cara_dimainkan2)
+print(alat2.jenis_alatmusik2)
