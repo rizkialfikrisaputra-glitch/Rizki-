e5063565458261a051e143c3d7e3b1737a6fc42e
rizkialfikrisaputra-glitch <rizkialfikrisaputra@gmail.com>
2026-06-18 22:47:13 +0700

    Add alatmusik class with instrument attributes

---
+++ Atribut setelah objek.py
@@ -0,0 +1,25 @@
+class alatmusik:
+   pass
+alat1=alatmusik()
+alat1.nama="gitar"
+alat1.asal="amerika"
+alat1.terbuat="kayu"
+alat1.caradimainkan="di genjreng atau dipetik"
+
+alat2=alatmusik()
+alat2.nama="drum"
+alat2.asal="jawa"
+alat2.terbuat="kulit"
+alat2.caradimainkan="dipukul"
+
+print("alat1:")
+print(alat1.nama)
+print(alat1.asal)
+print(alat1.terbuat)
+print(alat1.caradimainkan)
+print()
+print("alat2:")
+print(alat2.nama)
+print(alat2.asal)
+print(alat2.terbuat)
+print(alat2.caradimainkan)
