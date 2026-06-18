8360928bf31c9eb789603ed0218bab6e3c6e6d4d
rizkialfikrisaputra-glitch <rizkialfikrisaputra@gmail.com>
2026-06-18 22:55:12 +0700

    Add Nama.py with student information

---
+++ Nama.py
@@ -0,0 +1,7 @@
+#python 3.7.1
+
+print("no absen       = 11")
+print ("nama           = Fahmi Fauzan")
+print("kelas          = 11 rpl 1")
+print("mata pelajaran = PTGM")
+print("alamat         = desa cibodas rw 5")
