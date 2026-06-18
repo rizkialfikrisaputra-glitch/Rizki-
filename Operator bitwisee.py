ce77e5fc00ff7ee7c2c6d87e087f225c322a702f
rizkialfikrisaputra-glitch <rizkialfikrisaputra@gmail.com>
2026-06-18 22:53:35 +0700

    Implement bitwise operations in Python

---
+++ Operator bitwisee.py
@@ -0,0 +1,53 @@
+#operator bitwise
+a=6
+b=3
+
+
+c = a & b
+print ('nilai:',a)
+print ('nilai:',b,)
+print ('nilai bitwise AND:',c)
+ 
+ 
+#opertor or
+a=6
+b=3
+
+
+c= a|b
+print('nilai:',a,)
+print('nilai:',b,)
+print('nilai bitwise OR:',c,)
+
+
+c=~a
+c=~b
+print('nilai:',a,)
+print('nilai:',b,)
+print('nilai bitwise not:',c,)
+
+#operator bitwise
+a=6
+b=3
+
+
+c= a ^ b
+print('nilai:',a,)
+print ('nilai:',b,)
+print ('nilai bitwise XOR:',c,)
+ 
+ 
+#opertor or
+a=6
+b=3
+
+
+c= a>>2
+print('nilai:',a,)
+print('nilai:',b,)
+print('nilai bitwise shift right:',c,)
+ 
+c= a<<2
+print('nilai:',a,)
+print('nilai:',b,)
+print('nilai bitwise shift left:',c,)
