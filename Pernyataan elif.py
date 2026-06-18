75eb8b81cf9525e7357dcbf44a10796415d269bf
rizkialfikrisaputra-glitch <rizkialfikrisaputra@gmail.com>
2026-06-18 23:13:21 +0700

    Create Pernyataan elif.py

---
+++ Pernyataan elif.py
@@ -0,0 +1,10 @@
+nilai = int(input("Masukkan nilai Anda: "))
+
+if nilai >= 90:
+    print("A Lulus dengan baik")
+elif nilai >= 70:
+    print("B Lulus")
+elif nilai >= 50:
+    print("C Cukup")
+else:
+    print("D Tidak lulus")
